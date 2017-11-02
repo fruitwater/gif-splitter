0992f73533294d5fae02c203321770e3086973aa
silvird <snowland40@gmail.com>
2017-11-02 18:31:07 +0900

    commit31

--- src/main/java/com/gif/TopServlet.java
+++ src/main/java/com/gif/TopServlet.java
@@ -21,7 +21,7 @@ public class TopServlet extends BaseServlet{
 		fowardJsp(req, resp, "/WEB-INF/top.jsp");
 	}
        
-        public static void main(String args[]){
+        public static void main(String args[]) throws Exception{
 	Server server = new Server(Integer.valueOf(System.getenv("PORT")));
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.setContextPath("/");
